c0b59275fece7cd9d633b2f037a74ecc2aa96a6a
Thesamestaff <tss_works@riseup.net>
2025-12-16 20:16:56 -0600

    fix: Fix scrolling wall bug

--- natives/image/wall.cc
+++ natives/image/wall.cc
@@ -26,7 +26,12 @@ CmdOutput esmb::Image::Wall(const string &type, string &outType, const char *buf
   // We fit one tile inside a 128x128 box in order to
   // reduce resolution, then blow it up to 512x512
   double maxSize = std::max(width, pageHeight);
-  VImage tile = in.resize(128.0 / maxSize);
+  // We find a vertical scaling factor that ensures
+  // all pages have the same height. This prevents
+  // a weird scrolling effect that happens when we
+  // later crop out each page.
+  double vscale = (int)(pageHeight * (128.0 / maxSize)) / (double)pageHeight;
+  VImage tile = in.resize(128.0 / maxSize, VImage::option()->set("vscale", vscale));
   tile = tile.resize(512.0 / 128.0).copy_memory();
   int newHeight = tile.height();
   width = tile.width();
@@ -48,8 +53,6 @@ CmdOutput esmb::Image::Wall(const string &type, string &outType, const char *buf
   vector<VImage> img;
   for (int i = 0; i < nPages; i++) {
     VImage img_frame = nPages > 1 ? tile.crop(0, i * pageHeight, width, pageHeight) : tile;
-    // The 'extend' here doesn't do the tiling, it just fixes
-    // a weird border that forms around each tile.
     VImage frame = img_frame.mapim(index, VImage::option()->set("extend", VIPS_EXTEND_REPEAT));
     img.push_back(frame);
   }
